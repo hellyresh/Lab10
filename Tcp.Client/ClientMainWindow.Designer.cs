bdc07e9a09843247f6b9690ef695588517f90af1
hellyresh <hellyresh@gmail.com>
2020-06-02 18:33:58 +0300

    Добавьте файлы проекта.

---
+++ Tcp.Client/ClientMainWindow.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace SomeProject.TcpClient
+{
+    partial class ClientMainWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sendMsgBtn = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.labelRes = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.sendFileButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sendMsgBtn
+            // 
+            this.sendMsgBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.sendMsgBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sendMsgBtn.Font = new System.Drawing.Font("Segoe UI Symbol", 10F);
+            this.sendMsgBtn.Location = new System.Drawing.Point(3, 396);
+            this.sendMsgBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.sendMsgBtn.Name = "sendMsgBtn";
+            this.sendMsgBtn.Size = new System.Drawing.Size(369, 62);
+            this.sendMsgBtn.TabIndex = 0;
+            this.sendMsgBtn.Text = "Send Message";
+            this.sendMsgBtn.UseVisualStyleBackColor = false;
+            this.sendMsgBtn.Click += new System.EventHandler(this.OnMsgBtnClick);
+            // 
+            // textBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox, 2);
+            this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox.Location = new System.Drawing.Point(10, 10);
+            this.textBox.Margin = new System.Windows.Forms.Padding(10);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(730, 303);
+            this.textBox.TabIndex = 1;
+            // 
+            // labelRes
+            // 
+            this.labelRes.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.labelRes, 2);
+            this.labelRes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.labelRes.Location = new System.Drawing.Point(3, 323);
+            this.labelRes.Name = "labelRes";
+            this.labelRes.Size = new System.Drawing.Size(744, 69);
+            this.labelRes.TabIndex = 2;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.OnTimerTick);
+            // 
+            // sendFileButton
+            // 
+            this.sendFileButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.sendFileButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sendFileButton.Font = new System.Drawing.Font("Segoe UI Symbol", 10F);
+            this.sendFileButton.Location = new System.Drawing.Point(378, 396);
+            this.sendFileButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.sendFileButton.Name = "sendFileButton";
+            this.sendFileButton.Size = new System.Drawing.Size(369, 62);
+            this.sendFileButton.TabIndex = 3;
+            this.sendFileButton.Text = "Send File";
+            this.sendFileButton.UseVisualStyleBackColor = false;
+            this.sendFileButton.Click += new System.EventHandler(this.OnSendFileButtonClick);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelRes, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.sendFileButton, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.sendMsgBtn, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(750, 462);
+            this.tableLayoutPanel1.TabIndex = 4;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // ClientMainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 462);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(772, 518);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(772, 518);
+            this.Name = "ClientMainWindow";
+            this.Text = "Client application";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendMsgBtn;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label labelRes;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button sendFileButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
